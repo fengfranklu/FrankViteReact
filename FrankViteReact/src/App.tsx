48fa84243682b808e8dee7aa5ab7fbeac7cdfd00
fengfranklu <fengfranklu@gmail.com>
2025-04-29 16:09:29 -0400

    call back function part one

--- FrankViteReact/src/App.tsx
+++ FrankViteReact/src/App.tsx
@@ -2,16 +2,23 @@ import { dumyData } from "./data/todos";
 import TodoItem from "./components/TodoItem";
 function App() {
 
+  function setTodoCompleted (id: number, completed: boolean){
+    alert(
+      `Todo with id ${id} is now ${completed ? "coompleted" : "not completed"}`
+    )
+  }
+
   return (
-    <main className="py-10 h-screen">
+    <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
-      <div className="text-center py-10 max-w-lg mx-auto">
+      <div className="text-center py-10 max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
           {dumyData.map((todo) => (
-            <div key={todo.id} className="text-lg py-2">
-              <p>{todo.title}</p>
-              <p>{todo.completed ? 'Completed' : 'Not Completed'}</p>
-              <TodoItem todo={todo} />
+            <div>        
+              <TodoItem 
+              key={todo.id}
+              todo={todo}
+              onCompletedChange={setTodoCompleted} />
             </div>
           ))}
         </div>
