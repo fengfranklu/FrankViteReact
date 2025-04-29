4345959292ad65572d1dee0c8bb1dc7d0eba1b53
fengfranklu <fengfranklu@gmail.com>
2025-04-29 16:32:44 -0400

    callback function and useState

--- FrankViteReact/src/App.tsx
+++ FrankViteReact/src/App.tsx
@@ -1,19 +1,37 @@
 import { dumyData } from "./data/todos";
 import TodoItem from "./components/TodoItem";
+import { useState } from "react";
 function App() {
 
-  function setTodoCompleted (id: number, completed: boolean){
-    alert(
-      `Todo with id ${id} is now ${completed ? "coompleted" : "not completed"}`
-    )
+  const [todos, setTodos] = useState(dumyData);
+
+//   function setTodoCompleted (id: number, completed: boolean){
+//    setTodos((prevTodos) => {
+//     return prevTodos.map((todo) => {
+//       if (todo.id === id) {
+//         return { ...todo, completed };
+//       }
+//       return todo;
+//     })
+//   })
+// }
+
+  function setTodoCompleted (id:number, completed:boolean){
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed} : todo))
+    );
   }
+
+
+
+  
 
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div className="text-center py-10 max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
-          {dumyData.map((todo) => (
+          {todos.map((todo) => (
             <div>        
               <TodoItem 
               key={todo.id}
